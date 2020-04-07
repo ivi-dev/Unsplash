f031a943de8f2826a212783918937052f578ce3c
Iliyan Videv <videviliyan@gmail.com>
2020-04-07 19:45:21 +0300

    Added a CharExtensions class

---
+++ Library/Extensions/CharExtensions.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="CharExtensions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Unsplash.Extensions
+{
+    public static class CharExtensions
+    {
+        public static bool IsHyphenOrUnderscore(this char c)
+        {
+            return c.Equals('-') || c.Equals('_');
+        }
+    }
+}
---
+++ LibraryTests/Extensions/CharExtensionsTest.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="CharExtensionsTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace UnsplashTests.Extensions
+{
+    using System;
+    using global::Unsplash.Extensions;
+    using Xunit;
+
+    public class CharExtensionsTest
+    {
+        [Fact]
+        public void IsHyphenOrUnderscore_Hyphen_ReturnsTrue()
+        {
+            var character = '-';
+            bool result = character.IsHyphenOrUnderscore();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsHyphenOrUnderscore_Underscore_ReturnsTrue()
+        {
+            var character = '_';
+            bool result = character.IsHyphenOrUnderscore();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsHyphenOrUnderscore_Letter_ReturnsFalse()
+        {
+            var character = 'a';
+            bool result = character.IsHyphenOrUnderscore();
+            Assert.False(result);
+        }
+    }
+}

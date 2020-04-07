9f032700659ae3be443fe3b94795705a55991ee3
Iliyan Videv <videviliyan@gmail.com>
2020-04-07 12:57:19 +0300

    Added a Defaults class

---
+++ Library/Defaults.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Defaults.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Library
+{
+    using System;
+    using Unsplash.Photos;
+
+    public static class Defaults
+    {
+        public static uint PerPage { get; } = 10;
+
+        public static uint PerPageMax { get; } = 30;
+
+        public static uint Page { get; } = 1;
+
+        public static Order Order { get; } = Order.LATEST;
+    }
+}
